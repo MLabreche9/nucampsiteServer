c3081a354e6ea5ddd4758b4520ef53765e49fd85
Meghan La Breche <Megavictoria@gmail.com>
2021-01-20 20:32:46 -0800

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '743053226345277',
+        clientSecret: 'c23cbf4465a0c31ce5f5097a2107df3e'
+    }
 }
